e8dbfab25ec5a7b048b53585bf46432eeec3b32c
Gabriel dos Santos Vereda <gabrielsantosvereda@gmail.com>
2025-11-23 15:43:53 -0300

    refactor: remove unused button-fotos.js and optimize font loading in index.html

--- DataCenterSenai/assets/js/button-fotos.js
+++
@@ -1,9 +0,0 @@
-const botao = document.getElementById('meuBotao');
-
-botao.addEventListener('mouseover', () => {
-  botao.src = 'botao-hover.png';
-});
-
-botao.addEventListener('mouseout', () => {
-  botao.src = 'botao.png';
-});
